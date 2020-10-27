ed1c95fc321594c3cb4506f14e1f7f08c401302f
ARR0III <36902548+ARR0III@users.noreply.github.com>
2020-10-28 00:52:05 +0700

    Delete myfunctions.h

--- src/myfunctions.h
+++
@@ -1,6 +0,0 @@
-#include "myfunctions.c"
-
-int  genrand(const int min, const int max);
-int  readfromfile(const char * filename, uint8_t * buffer, const size_t length);
-void strxor(uint8_t * one, const uint8_t * two, const size_t length);
-void printhex(const int tumbler, const uint8_t * data, const size_t length);
