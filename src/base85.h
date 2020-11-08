65fca1ad54395f7076f9ac26bebc5620fb003364
ARR0III <36902548+ARR0III@users.noreply.github.com>
2020-11-09 01:07:52 +0700

    Add files via upload

---
+++ src/base85.h
@@ -0,0 +1,7 @@
+#include "base85.c"
+
+int32_t base85_decode_len(int32_t len); /* atob */
+int32_t base85_encode_len(int32_t len); /* btoa */
+
+int base85_encode(const uint8_t * input, int inlen, uint8_t * output);
+int base85_decode(const uint8_t * input, int inlen, uint8_t * output);
