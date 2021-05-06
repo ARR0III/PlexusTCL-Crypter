3ccb4d13fcf6bce92f5c3f716e401c19c840e1d5
ARR0III <36902548+ARR0III@users.noreply.github.com>
2021-05-07 03:16:22 +0700

    Delete test_src directory

--- test_src/base64.h
+++
@@ -1,4 +0,0 @@
-#include "base64.c"
-
-void base64encode(const uint8_t * input, uint8_t * output, const int size);
-void base64decode(const uint8_t * input, uint8_t * output, const int size);
--- test_src/crc32.h
+++
@@ -1,3 +0,0 @@
-#include "crc32.c"
-
-uint32_t CRC32(const uint8_t * buffer, uint64_t len);
