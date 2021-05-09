9e4fdb5ba2891ce1620341f46f23d0acfe8c9c7d
ARR0III <36902548+ARR0III@users.noreply.github.com>
2021-05-09 21:53:08 +0700

    Delete test_src directory

--- test_src/rijndael.h
+++
@@ -1,7 +0,0 @@
-#include "rijndael.c"
-
-int rijndael_key_encrypt_init(uint32_t * table, const uint8_t * cipher_key, int key_bits);
-int rijndael_key_decrypt_init(uint32_t * table, const uint8_t * cipher_key, int key_bits);
-
-void rijndael_encrypt(const uint32_t * table, const uint8_t * pt, uint8_t * ct);
-void rijndael_decrypt(const uint32_t * table, const uint8_t * ct, uint8_t * pt);
