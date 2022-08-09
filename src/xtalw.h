91bd83cc4fe702a09c00535dbd95d1e660f03289
ARR0III <36902548+ARR0III@users.noreply.github.com>
2022-08-10 04:38:03 +0700

    Update xtalw.h

--- src/xtalw.h
+++ src/xtalw.h
@@ -29,6 +29,7 @@ size_t printhex(const int tumbler, const void * data, size_t length);
 size_t x_strnlen(const char * string, size_t length);
 
 void * meminit(void * data, const uint8_t simbol, size_t length);
+void * strxormove(void * output, const void * input, size_t length);
 void * strxor(uint8_t * output, const uint8_t * input, size_t length);
 
 #ifdef __cplusplus
