789e32c62d08974a9b6f2182c35e880c2c4378e4
ARR0III <36902548+ARR0III@users.noreply.github.com>
2022-08-10 05:18:21 +0700

    Add files via upload

--- src/xtalw.c
+++ src/xtalw.c
@@ -13,11 +13,11 @@
 #define HEX_TABLE  1
 #define HEX_STRING 0
 
-void chartobits(uint8_t * data, int len, FILE * stream) {
+void chartobits(uint8_t * data, size_t len, FILE * stream) {
   if (!data)
     return;
 
-  for (int j = 0; j < len; j++) {
+  for (size_t j = 0; j < len; j++) {
     uint8_t c = data[j];
 
     for(int k = 7; k >= 0; --k) {
@@ -29,7 +29,7 @@ void chartobits(uint8_t * data, int len, FILE * stream) {
     putc('\n', stream);
 }
 
-void strinc(uint8_t * data, int len) {
+void strinc(uint8_t * data, size_t len) {
   while(--len) {
     if (0xFF == data[len]) {
       data[len] = 0x00;
@@ -42,7 +42,7 @@ void strinc(uint8_t * data, int len) {
   }
 }
 
-void strdec(uint8_t * data, int len) {
+void strdec(uint8_t * data, size_t len) {
   while(--len) {
     if (0x00 == data[len]) {
       data[len] = 0xFF;
@@ -109,111 +109,6 @@ int readfromfile(const char * filename, void * buffer, const size_t length) {
   return result;
 }
 
-void * strxor_un(void * out, const void * in, size_t lenght) {
-  if (!out || !in || (out == in)) {
-    return out;
-  }
-
-        unsigned char * local_out = (unsigned char *)out;
-  const unsigned char * local_in  = (const unsigned char *)in;
-
-        uint32_t * local_32_out = (uint32_t *)local_out;
-  const uint32_t * local_32_in  = (uint32_t *)local_in;
-
-        uint64_t * local_64_out = (uint64_t *)local_out;
-  const uint64_t * local_64_in  = (uint64_t *)local_in;
-
-  size_t register_size = sizeof(size_t);
-
-  if (8 == register_size) {
-    if (local_64_out < local_64_in) {
-      while (lenght) {
-
-        if (lenght < register_size)
-          break;
-
-        *local_64_out ^= *local_64_in;
-      
-        ++local_64_out;
-        ++local_64_in;
-
-        lenght -= register_size;
-      }
-    }
-    else {
-            uint64_t * last_64_out = (uint64_t *)(local_out + (lenght - 1));
-      const uint64_t * last_64_in  = (uint64_t *)(local_in  + (lenght - 1));
-      while (lenght) {
-
-        if (lenght < register_size)
-          break;
-
-        *last_64_out ^= *last_64_in;
-      
-        --last_64_out;
-        --last_64_in;
-
-        lenght -= register_size;
-      }
-    }
-  }
-  else
-  if (4 == register_size) {
-    if (local_32_out < local_32_in) {
-      while (lenght) {
-
-        if (lenght < register_size)
-          break;
-
-        *local_32_out ^= *local_32_in;
-      
-        ++local_32_out;
-        ++local_32_in;
-
-        lenght -= register_size;
-      }
-    }
-    else {
-            uint32_t * last_32_out = (uint32_t *)(local_out + (lenght - 1));
-      const uint32_t * last_32_in  = (uint32_t *)(local_in  + (lenght - 1));
-      while (lenght) {
-
-        if (lenght < register_size)
-          break;
-
-        *last_32_out ^= *last_32_in;
-      
-        --last_32_out;
-        --last_32_in;
-
-        lenght -= register_size;
-      }
-    }
-  }
-
-  if (local_out < local_in) {
-    while(lenght--) {
-      *local_out ^= *local_in;
-
-      ++local_out;
-      ++local_in;
-    }
-  }
-  else {
-          unsigned char * last_out = local_out + (lenght - 1);
-    const unsigned char * last_in  = local_in  + (lenght - 1);
-     
-     while (lenght--) {
-       *last_out ^= *last_in;
-
-       --last_out;
-       --last_in;
-     }
-  }
-
-  return out;
-}
-
 void * strxormove(void * output, const void * input, size_t length) {
   if (!output || !input || (output == input)) {
     return output;
@@ -274,12 +169,10 @@ void phex(int tumbler, const uint8_t * data, size_t length, FILE * stream) {
     stream = stderr;
   }
 
-  size_t i;
-
   int left, right, symbol;
   const char digits[] = "0123456789ABCDEF";
 
-  for (i = 0; i < length; ++i) {
+  for (size_t i = 0; i < length; ++i) {
     symbol = (int)data[i];
 
     left  = symbol >> 0x04; /* 11000011 >> 0x04 = 00001100 */
