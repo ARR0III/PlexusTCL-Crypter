bf8ad633f54e7a7532be64f84a39b63e12199d98
ARR0III <36902548+ARR0III@users.noreply.github.com>
2023-01-29 03:27:04 +0700

    Update PlexusTCL_Crypter.cpp

--- PlexusTCL_Crypter.cpp
+++ PlexusTCL_Crypter.cpp
@@ -1,5 +1,4 @@
 //---------------------------------------------------------------------------
-
 #include <vcl.h>
 #pragma hdrstop
 //---------------------------------------------------------------------------
@@ -7,11 +6,12 @@ USEFORM("Unit1.cpp", Form1);
 USEFORM("Unit2.cpp", Form2);
 //---------------------------------------------------------------------------
 WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+  
   try {
     Application->Initialize();
     Application->CreateForm(__classid(TForm1), &Form1);
-                 Application->CreateForm(__classid(TForm2), &Form2);
-                 Application->Run();
+    Application->CreateForm(__classid(TForm2), &Form2);
+    Application->Run();
   }
   catch (Exception &exception) {
     Application->ShowException(&exception);
