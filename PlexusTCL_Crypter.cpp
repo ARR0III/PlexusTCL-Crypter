875795a177b26cbc535091d9ab1f24871349744c
ARR0III <36902548+ARR0III@users.noreply.github.com>
2023-01-29 04:02:59 +0700

    Update PlexusTCL_Crypter.cpp

--- PlexusTCL_Crypter.cpp
+++ PlexusTCL_Crypter.cpp
@@ -10,8 +10,8 @@ WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
   try {
     Application->Initialize();
     Application->CreateForm(__classid(TForm1), &Form1);
-                 Application->CreateForm(__classid(TForm2), &Form2);
-                 Application->Run();
+    Application->CreateForm(__classid(TForm2), &Form2);
+    Application->Run();
   }
   catch (Exception &exception) {
     Application->ShowException(&exception);
