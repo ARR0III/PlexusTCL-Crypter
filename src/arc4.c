48648a2c2c7db726ebc8e4036f337f326bc1b6b4
ARR0III <36902548+ARR0III@users.noreply.github.com>
2023-02-12 00:56:15 +0700

    Add files via upload

--- src/arc4.c
+++ src/arc4.c
@@ -1,13 +1,7 @@
 #include "arc4.h"
 
-void swap (uint8_t * a, uint8_t * b) {
-  uint8_t t = *a;
-
-  *a = *b;
-  *b = t;
-}
-
 void arc4_init(ARC4_CTX * ctx, const uint8_t * key, const size_t length) {
+  uint8_t t;
 
   for (ctx->i = 0; ctx->i < 256; ctx->i++) {
     ctx->secret_key[ctx->i] = (uint8_t)(ctx->i);
@@ -15,7 +9,12 @@ void arc4_init(ARC4_CTX * ctx, const uint8_t * key, const size_t length) {
  
   for (ctx->i = ctx->j = 0; ctx->i < 256; ctx->i++) {
     ctx->j = (ctx->j + key[ctx->i % length] + ctx->secret_key[ctx->i]) & 255;
-    swap(&ctx->secret_key[ctx->i], &ctx->secret_key[ctx->j]);
+
+     
+    t = ctx->secret_key[ctx->i];
+    ctx->secret_key[ctx->i] = ctx->secret_key[ctx->j];
+    ctx->secret_key[ctx->j] = t;
+    
   }
 
   ctx->i = ctx->j = 0;
@@ -23,13 +22,16 @@ void arc4_init(ARC4_CTX * ctx, const uint8_t * key, const size_t length) {
 
 /* MAX size data for encrypt = 4 GiB */
 void arc4(ARC4_CTX * ctx, const uint8_t * input, uint8_t * output, size_t length) {
-  register uint32_t position = 0;
+  uint8_t t;  
+  register size_t position = 0;
   
   while (length--) {
     ctx->i = (ctx->i + 1) & 255;
     ctx->j = (ctx->j + ctx->secret_key[ctx->i]) & 255;
     
-    swap(&ctx->secret_key[ctx->i], &ctx->secret_key[ctx->j]);
+    t = ctx->secret_key[ctx->i];
+    ctx->secret_key[ctx->i] = ctx->secret_key[ctx->j];
+    ctx->secret_key[ctx->j] = t;
     
     output[position] = 
       input[position] ^ ctx->secret_key[(ctx->secret_key[ctx->i] +
