d45d43cc0b5e3177fa97400560779e227049e192
ARR0III <xtalw.ztax@gmail.com>
2024-07-29 01:13:48 +0700

    Update crycon_compile.sh

--- crycon_compile.sh
+++ crycon_compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-gcc -Wall -D_FILE_OFFSET_BITS=64 -O0 -c crycon.c
+gcc -Wall -D_FILE_OFFSET_BITS=64 -O0 -c crycon_linux.c
 cd src/
 clang -Wall -O3 -c *.c
 cd ..
-clang -Wall -static src/*.o crycon.o -o crycon
+clang -Wall -static src/*.o crycon_linux.o -o crycon
