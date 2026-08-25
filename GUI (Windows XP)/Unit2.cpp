0010e8ca3ce37aae4a8cb5722a91fc89c24836a7
ARR0III <xtalw.ztax@gmail.com>
2026-08-25 20:05:48 +0700

    Add files via upload

---
+++ GUI (Windows XP)/Unit2.cpp
@@ -0,0 +1,48 @@
+#include <vcl.h>
+#pragma hdrstop
+
+#include <stdint.h>
+
+#include "Unit2.h"
+
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+
+TForm2 *Form2;
+
+__fastcall TForm2::TForm2(TComponent* Owner): TForm(Owner) {
+}
+
+void __fastcall TForm2::Label5Click(TObject *Sender) {
+  Form2->Close();        
+}
+
+void __fastcall TForm2::Shape1MouseDown(TObject *Sender,
+      TMouseButton Button, TShiftState Shift, int X, int Y) {
+  ReleaseCapture();
+  Form2->Perform(WM_SYSCOMMAND, 0xF012, 0);        
+}
+//---------------------------------------------------------------------------
+
+
+
+
+void __fastcall TForm2::Label5MouseEnter(TObject *Sender) {
+  Label5->Font->Color = TColor((~(uint32_t)Label5->Font->Color) & 0x00FFFFFF);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm2::Label5MouseLeave(TObject *Sender) {
+  Label5->Font->Color = TColor((~(uint32_t)Label5->Font->Color) & 0x00FFFFFF);       
+}
+//---------------------------------------------------------------------------
+
+
+void __fastcall TForm2::Label4MouseDown(TObject *Sender,
+      TMouseButton Button, TShiftState Shift, int X, int Y)
+{
+  ReleaseCapture();
+  Form2->Perform(WM_SYSCOMMAND, 0xF012, 0);          
+}
+//---------------------------------------------------------------------------
+
